cfa07a32b8d43f891fd92a64a41dfab163ad91b8
Alex Johnston <alexjohnston@ymail.com>
2019-09-10 21:09:18 +0100

    initial API commit

---
+++ src/adapters/API.js
@@ -0,0 +1,19 @@
+let SEARCH_URL = ''
+
+if (window.location.href.includes('localhost')) {
+  SEARCH_URL = 'http://localhost:3000';
+} else {
+  SEARCH_URL = 'https://api.smarkets.com/v3/events/?state=upcoming&type=';
+}
+
+const searchEvents = async (query) => {
+  const res = await fetch(`${SEARCH_URL}/${query}`)
+  return res.json()
+}
+
+const searchOptions = ['american_football_match', 'american_football_outright', 'baseball_match', 'baseball_outright', 'basketball_match', 'boxing_match', 'cricket_match', 'cricket_outright', 'current_affairs', 'cycling', 'darts_match', 'darts_outright', 'football_match', 'football_outright', 'golf_match', 'golf_outright', 'handball_match', 'horse_racing_race', 'ice_hockey_match', 'mma_match', 'motorsports_race', 'motorsports_outright', 'politics', 'politics_outright', 'rowing', 'rugby_league_match', 'rugby_league_outright', 'rugby_union_match', 'rugby_union_outright', 'snooker_match', 'snooker_outright', 'tennis_match', 'tennis_outright', 'volleyball_match', 'generic', 'top_level_event', 'tv_entertainment']
+
+export default {
+  searchEvents,
+  searchOptions
+}
